0fa9addad1be02b2ccb51ed1f50beed715167008
JeremiahMutuc <jeremiahmutuc_p@yahoo.com>
2019-08-12 10:26:11 +0800

    print all objects in fetched link

--- fetch.py
+++ fetch.py
@@ -5,3 +5,5 @@ req = requests.get('https://jsonplaceholder.typicode.com/todos')
 print(req) #numbers of attributes
 
 print(dir(req)) #print all 200 attributes
+
+print(req.json()) #print all objects in fetched link
